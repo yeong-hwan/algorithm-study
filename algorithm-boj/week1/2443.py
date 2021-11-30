5cd6b6b7614f22b20c7c89a8803482dbd34906e3
yeonghwan <worry2much@yonsei.ac.kr>
2021-11-30 23:50:15 +0900

    Solve B3 problem 2444 / python

--- algorithm-boj/week1/2443.py
+++ algorithm-boj/week1/2443.py
@@ -3,5 +3,10 @@ input = stdin.readline
 
 N = int(input())
 
-for i in range(1, N+1):
-    print(' ' * (i-1) + '*' * (2*(N-i) + 1))
+for i in range(1, N):
+    print(' ' * (N-i) + '*' * (2*i - 1))
+
+print('*' * (2*N - 1))
+
+for i in range(1, N):
+    print(' ' * i + '*' * (2*(N-i) - 1))
