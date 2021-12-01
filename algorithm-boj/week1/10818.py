6843b25bac9c5dc825e8d2554f39807808fb5ba6
yeonghwan <worry2much@yonsei.ac.kr>
2021-12-01 11:16:21 +0900

    Solve B2 problem 10809 / python

--- algorithm-boj/week1/10818.py
+++ algorithm-boj/week1/10818.py
@@ -1,6 +1,11 @@
-from sys import stdin
+from sys import stdin, stdout
 input = stdin.readline
+print = stdout.write
 
-N = int(input())
-number_sets = [int(number) for number in input().split()]
-print(max(number_sets), min(number_sets))
+S = input()
+
+for i in range(97, 123):
+    if chr(i) in S:
+        print(f'{S.index(chr(i))} ')
+    else:
+        print('-1 ')
