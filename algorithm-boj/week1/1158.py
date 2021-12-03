adacf896ff751b0ecd0523b314360516b502dee5
yeonghwan <worry2much@yonsei.ac.kr>
2021-12-03 22:40:13 +0900

    Solve S5 problem 1158 / python

--- algorithm-boj/week1/1158.py
+++ algorithm-boj/week1/1158.py
@@ -3,12 +3,12 @@ input = stdin.readline
 
 N, K = map(int, input().split())
 queue = [i for i in range(1, N+1)]
-order_number = K-1
 answer_queue = []
+previous_index = K-1
 
-while len(queue) != 0:
-    if order_number+1 <= len(queue):
-        answer_queue.append(queue[order_number])
-        order_number += K
-    # else:
-        # order_number %=
+for _ in range(N):
+    if previous_index >= len(queue):
+        previous_index = previous_index % len(queue)
+    answer_queue.append(str(queue.pop(previous_index)))
+    previous_index += K-1
+print(f'<{", ".join(answer_queue)}>')
