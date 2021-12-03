1969b56e983a8c72446ad822d058b99c485bf2dc
yeong-hwan <jghff700@naver.com>
2021-12-03 17:57:29 +0900

    Upload BOJ 1158

--- algorithm-boj/week1/1158.py
+++ algorithm-boj/week1/1158.py
@@ -2,5 +2,13 @@ from sys import stdin
 input = stdin.readline
 
 N, K = map(int, input().split())
-
 queue = [i for i in range(1, N+1)]
+order_number = K-1
+answer_queue = []
+
+while len(queue) != 0:
+    if order_number+1 <= len(queue):
+        answer_queue.append(queue[order_number])
+        order_number += K
+    # else:
+        # order_number %=
