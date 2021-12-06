1775157c44a959455efa088ee8bae3938d6daba3
yeong-hwan <jghff700@naver.com>
2021-12-07 01:46:18 +0900

    Solve S3 problem 1966 / python

---
+++ algorithm-boj/week1/11279.py
@@ -0,0 +1,4 @@
+from sys import stdin
+input = stdin.readline
+
+T = int(input())
--- algorithm-boj/week1/1966.py
+++ algorithm-boj/week1/1966.py
@@ -6,11 +6,19 @@ T = int(input())
 for _ in range(T):
     # num_doc, check_loaction
     N, M = map(int, input().split())
-    # desc
-    queue_importance = list(map(int, input().split()))
-    queue, i = [], 1
-    for importance in queue_importance:
-        queue.append((i, importance))
-        i += 1
-    queue.sort(key=lambda x: (-x[1], -x[0]))
-    print(queue)
+    queue = list(map(int, input().split()))
+    index = [0 for _ in range(N)]
+    index[M] = 1
+    count = 0
+
+    while queue:
+        if queue[0] == max(queue):
+            count += 1
+            if index[0] == 1:
+                print(count)
+                break
+            queue.pop(0)
+            index.pop(0)
+        else:
+            queue.append(queue.pop(0))
+            index.append(index.pop(0))
