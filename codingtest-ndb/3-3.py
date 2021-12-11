7edd2525ccaa9d4de8325ee2dab299e23e4fdde9
yeonghwan <worry2much@yonsei.ac.kr>
2021-12-12 04:30:54 +0900

    Solve codingtest-ndb 3-3 / python

--- codingtest-ndb/3-3.py
+++ codingtest-ndb/3-3.py
@@ -2,8 +2,10 @@ from sys import stdin
 input = stdin.readline
 
 N, M, K = map(int, input().split())
-print(N, M, K)
 num_list = list(map(int, input().split()))
+num_list.sort()
 
-print(N)
-print(num_list)
+first = num_list[-1]
+second = num_list[-2]
+
+print((M // (K+1)) * (K*first + second) + (M % (K+1)) * first)
