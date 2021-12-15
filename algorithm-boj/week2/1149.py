08a6ac9c95a329237d2e97898e8a96f80a123cae
yeong-hwan <jghff700@naver.com>
2021-12-15 20:02:25 +0900

    Upload S2 problem 1260 / python

--- algorithm-boj/week2/1149.py
+++ algorithm-boj/week2/1149.py
@@ -1,4 +1,3 @@
-from collections import deque
 from sys import stdin
 input = stdin.readline
 
---
+++ algorithm-boj/week2/1260.py
@@ -0,0 +1,41 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+
+N, M, V = map(int, input().split())
+visited = [False] * (N+1)
+graph = [[] for _ in range(M+1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for node in range(1, N+1):
+    graph[node].sort()
+
+
+def dfs(node):
+    visited[node] = True
+    print(node, end=' ')
+    for i in graph[node]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(node):
+    queue = deque([node])
+    visited[node] = True
+    while queue:
+        node_now = queue.popleft()
+        print(node_now, end=' ')
+        for i in graph[node_now]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+dfs(V)
+visited = [False] * (N+1)
+print()
+bfs(V)
