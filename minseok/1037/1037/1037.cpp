070458634ce6cc160345f0724ccc9a2750911dea
Minseok-colli <minsock6@naver.com>
2021-12-26 14:16:56 +0900

    Move minseok's directory

---
+++ minseok/1037/1037/1037.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int* input = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> input[n];
+	}
+	sort(input, input + n);
+	cout << input[0] * input[n - 1];
+	return 0;
+}
