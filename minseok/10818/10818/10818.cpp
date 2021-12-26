4837ca6c8e6b80eaccab99b42405bd3a504ecadc
Minseok-colli <minsock6@naver.com>
2021-12-27 00:33:35 +0900

    Solved 10818 / c++

---
+++ minseok/10818/10818/10818.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n, num;
+    int min = 1000001, max = -1000001;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> num;
+        if (num > max)
+            max = num;
+        if (num < min)
+            min = num;
+    }
+    printf("%d %d", min, max);
+
+    return 0;
+}
