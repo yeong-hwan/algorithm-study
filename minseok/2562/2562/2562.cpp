7e93b7d7e315c03d92412c9492791ed561a83e38
Minseok-colli <minsock6@naver.com>
2021-12-26 22:39:42 +0900

    Solved 2562

---
+++ minseok/2562/2562/2562.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int input[9];
+	int MaxValue = 0, MaxIndex = 0;
+	for (int i = 0; i < 9; i++) {
+		cin >> input[i];
+		if (MaxValue < input[i]) {
+			MaxValue = input[i];
+			MaxIndex = i;
+		}
+	}
+	cout << MaxValue << endl << MaxIndex + 1;
+}
