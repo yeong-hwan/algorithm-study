82fd7afaf1ee3b09622889bd7edea3672bb3a0d1
Minseok-colli <minsock6@naver.com>
2021-12-26 23:29:14 +0900

    Solved 2576 / C++

---
+++ minseok/2576/2576/2576.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int input[7];
+	int sum = 0, min = 500, cnt=0;
+
+	for (int i = 0; i < 7; i++) {
+		cin >> input[i];
+		if (input[i] % 2 == 1) {
+			sum += input[i];
+			if (input[i] < min) {
+				min = input[i];
+				cnt++;
+			}
+		}
+	}
+	if (cnt == 0)
+		cout << "-1";
+	else
+		cout << sum << endl << min;
+}
