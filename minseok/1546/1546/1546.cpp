cde12e276f7ce2a44a68da2a690ed17c27552509
Minseok-colli <minsock6@naver.com>
2021-12-27 00:16:04 +0900

    Solved 1546 / c++

---
+++ minseok/1546/1546/1546.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num, index;
+	int Max = 0;
+	float score[1000], sum = 0;
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+		cin >> score[i];
+		if (Max < score[i]) {
+			Max = score[i];
+			index = i;
+		}
+	}
+
+	for (int i = 0; i < num; i++) {
+		score[i] = score[i] / Max * 100;
+	}
+	score[num] = Max;
+
+	for (int i = 0; i < num; i++) {
+		sum += score[i];
+	}
+
+	cout << sum / num;
+}
