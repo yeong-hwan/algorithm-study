e3f3d53fb83a69931406eeeb33b319e507aa1f4c
Minseok-colli <minsock6@naver.com>
2021-12-27 20:31:03 +0900

    solved 10866 / c++

---
+++ minseok/10866/10866/10866.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	int data;
+	deque<int> DQ;
+	string command;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> command;
+
+		if (command == "push_front") {
+			cin >> data;
+			DQ.push_front(data);
+		}
+
+		else if (command == "push_back") {
+			cin >> data;
+			DQ.push_back(data);
+		}
+
+		else if (command == "pop_front") {
+			if (DQ.empty()) cout << "-1" << endl;
+			else {
+				cout << DQ.front() << endl;
+				DQ.pop_front();
+			}
+		}
+
+		else if (command == "pop_back") {
+			if (DQ.empty()) cout << "-1" << endl;
+			else {
+				cout << DQ.back() << endl;
+				DQ.pop_back();
+			}
+		}
+
+		else if (command == "size") {
+			cout << DQ.size() << endl;
+		}
+
+		else if (command == "empty") {
+			if (DQ.empty()) cout << 1;
+			else cout << -1;
+		}
+
+		else if (command == "front") {
+			if (DQ.empty()) cout << "-1" << endl;
+			else cout << DQ.front();
+		}
+
+		else if (command == "back") {
+			if (DQ.empty()) cout << "-1" << endl;
+			else cout << DQ.back();
+		}
+	}
+	return 0;
+}
