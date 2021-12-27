6f3cb7f54e30a93b0bec93eb458c492d7e1fec3b
Minseok-colli <minsock6@naver.com>
2021-12-27 11:03:37 +0900

    Solved 3460 / c++

---
+++ minseok/3460/3460/3460.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int T, n;
+
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> n;
+		int bin = 0;
+		while (n > 0) {
+			if (n % 2 == 1)
+				cout << bin << " ";
+			n /= 2;
+			bin++;
+		}
+	}
+}
