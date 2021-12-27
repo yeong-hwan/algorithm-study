8bd7770026334f087f67486450d9b863dc473569
Minseok-colli <minsock6@naver.com>
2021-12-27 19:40:25 +0900

    Solved 10828 / c++

---
+++ minseok/10828/10828/10828.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+stack<int> S;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string command;
+        cin >> command;
+
+        if (command == "push") {
+            int x; 
+            cin >> x;
+            S.push(x);
+        }
+
+        else if (command == "top") {
+            if (S.empty())cout << "-1" << endl;
+            else cout << S.top() << "\n";
+        }
+        else if (command == "pop") {
+            if (S.empty())cout << "-1" << endl;
+            else {
+                cout << S.top() << endl;
+                S.pop();
+            }
+        }
+        else if (command == "empty") {
+            cout << S.empty() << endl;
+        }
+        else {
+            cout << S.size() << endl;
+        }
+    }
+    return 0;
+}
