2c3c4f89299d07867fa2cf8c81786e791c49616c
Minseok-colli <minsock6@naver.com>
2022-02-07 21:25:02 +0900

    solved 1149/C++

--- minseok/1149/1149.cpp
+++ minseok/1149/1149.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include <vector>
-
+#include <algorithm>
+    
 using namespace std;
 
 int main(){
     int T;
-    vector<vector<int>> House;
-    vector<int> Colored;
-
-    cin >> T;
+    int House[1001][3] = {0,};
     
+    cin >> T;
+    for(int i = 0 ; i < T ; i++){
+        cin >> House[i][0] >> House[i][1] >> House[i][2];
 
-       
+        House[i][0] += min(House[i-1][1],House[i-1][2]);
+        House[i][1] += min(House[i-1][0],House[i-1][2]);
+        House[i][2] += min(House[i-1][0],House[i-1][1]);
+    }
+    int result = min(House[T][0], min(House[T][1],House[T][2]));
+    cout << result;
 }
