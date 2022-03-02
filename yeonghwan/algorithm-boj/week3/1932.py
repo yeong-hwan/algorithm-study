87ac0a4dbb7bae7b5fc942d5ddea5f7839c26ea3
yeong-hwan <jghff700@naver.com>
2022-03-02 21:43:49 +0900

    Solve some problems

--- yeonghwan/algorithm-boj/week3/1932.py
+++ yeonghwan/algorithm-boj/week3/1932.py
@@ -1,17 +1,13 @@
 from sys import stdin
 input = stdin.readline
 
-T = int(input())
+N = int(input())
 
+triangle_list = []
 
-for _ in range(T):
-    N = int(input())
-    if N < 4:
-        print(1)
-    elif N < 6:
-        print(2)
-    else:
-        a, b, c, d, e = 1, 1, 1, 2, 2
-        for i in range(6, N+1):
-            a, b, c, d, e = b, c, d, e, e+a
-        print(e)
+for i in range(N):
+    triangle_list.append(list(map(int, input().split())))
+for i in range(N):
+    triangle_list[i].insert(0, 0)
+
+print(triangle_list)
