e991023a65cc437f4ea7a7e5aefdfad22807646c
yeong-hwan <jghff700@naver.com>
2022-10-10 08:44:44 +0900

    checkBoard update

--- yeonghwan/clisp/sudoku.lisp
+++ yeonghwan/clisp/sudoku.lisp
@@ -22,14 +22,39 @@
                 (dotimes (i 9 t)
                     (when (or (= (+ num 1) (aref board row i))
                             (= (+ num 1) (aref board i col))
-                            (= (+ num 1) (aref board (+ r (mod i 3))
-                                (+ c (truncate i 3)))))
+                            (= (+ num 1) (aref board (+ r (mod i 3)) (+ c (truncate i 3))))
+                            )
                         (return nil)
                     )
                 )
         )
     )
 )
+
+
+(boardPrint #2a((1 3 5 4 6 9 2 7 8) (7 8 2 1 3 5 6 4 9) (4 6 9 2 7 8 1 3 5)
+(3 2 1 5 4 6 8 9 7) (8 7 4 9 1 3 5 2 6) (5 9 6 8 2 7 4 1 3) (9 1 7 6 5 2 3
+8 4) (6 4 3 7 8 1 9 5 2) (2 5 8 3 9 4 7 6 1)) 3 3)
+
+
+(defparameter board #2a((1 6 0 1 0 4 0 5 0)
+                        (0 0 8 3 0 5 6 0 0)
+                        (2 0 0 0 0 0 0 0 1)
+                        (8 0 0 4 0 7 0 0 6)
+                        (0 0 6 0 0 0 3 0 0)
+                        (7 0 0 9 0 1 0 0 4)
+                        (5 0 0 0 0 0 0 0 2)
+                        (0 0 7 2 0 6 9 0 0)
+                        (0 4 0 5 0 8 0 7 1)))
+
+(aref board 0 1) ; 6
+(aref board 0 7) ; 5
+
+
+
+(defun boardPrint (board col row)
+    (aref board 0 1)
+    (aref board 0 2))
 
 
 
@@ -42,9 +67,6 @@
 (checkBoard #2a((5 3 0 0 7 0 0 0 0) (6 0 0 1 9 5 0 0 0) (0 9 8 0 0 0 0 6 0)
 (8 0 0 0 6 0 0 0 3) (4 0 0 8 0 3 0 0 1) (7 0 0 0 2 0 0 0 6) (0 6 0 0 0 0 2 8 0)
 (0 0 0 4 1 9 0 0 5) (0 0 0 0 8 0 0 7 9)) 3 3 )
-
-
-
 
 
 
