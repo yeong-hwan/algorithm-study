c8e12f53aa842706a57f4a8a2a94c97d39507008
yeonghwan <worry2much@yonsei.ac.kr>
2022-10-12 04:03:44 +0900

    Change file name

--- yeonghwan/clisp/2019145019.lisp
+++ yeonghwan/clisp/2019145019.lisp
@@ -57,6 +57,7 @@
 (3 2 1 5 4 6 8 9 7) (8 7 4 9 1 3 5 2 6) (5 9 6 8 2 7 4 1 3) (9 1 7 6 5 2 3
 8 4) (6 4 3 7 8 1 9 5 2) (2 5 8 3 9 4 7 6 1)) 7 8) 
 
+; 3. solveBoard
 (defun solveBoard (board)
   (let ((filled t) x y)
     (dotimes (i 9)
@@ -64,19 +65,17 @@
         (when (= (aref board i j) 0)
           (setf filled nil x i y j))))
     (if filled
-        ; (describe board)
         (printBoard board)
         (dotimes (k 9)
           (setf (aref board x y) (1+ k))
           (when (checkBoard board x y)
             (solveBoard board))
-          (setf (aref board x y) 0)))))
+          ; (setf (aref board x y) 0)))))
 
 
-; (solveBoard #2a((0 3 5 4 6 9 2 7 8) (7 8 2 1 0 5 6 0 9) (0 6 0 2 7 8 1 3 5)
-; (3 2 1 0 4 6 8 9 7) (8 0 4 9 1 3 5 0 6) (5 9 6 8 2 0 4 1 3) (9 1 7 6 5 2 0
-; 8 0) (6 0 3 7 0 1 9 5 2) (2 5 8 3 9 4 7 6 0)))
-
+(solveBoard #2a((0 3 5 4 6 9 2 7 8) (7 8 2 1 0 5 6 0 9) (0 6 0 2 7 8 1 3 5)
+(3 2 1 0 4 6 8 9 7) (8 0 4 9 1 3 5 0 6) (5 9 6 8 2 0 4 1 3) (9 1 7 6 5 2 0
+8 0) (6 0 3 7 0 1 9 5 2) (2 5 8 3 9 4 7 6 0)))
 
 (solveBoard #2a((0 3 5 4 6 9 2 7 8) (7 8 2 1 0 5 6 0 9) (0 6 0 2 7 8 1 3 5)
 (3 2 1 0 4 6 8 9 7) (8 0 4 9 1 3 5 0 6) (5 9 6 8 2 0 4 1 3) (9 1 7 6 5 2 0
